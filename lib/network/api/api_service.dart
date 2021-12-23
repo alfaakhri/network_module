a9aa38690cb267f5169bd82305d347488d00052d
Said Al fakhri <alfakhri1998@gmail.com>
2021-12-23 16:39:43 +0700

    edit message exception

--- lib/network/api/api_service.dart
+++ lib/network/api/api_service.dart
@@ -21,13 +21,13 @@ class ApiService {
       if (response.statusCode == 200) {
         return PhotosModel.fromJson(response.data);
       } else {
-        throw Exception('Failed to load list restaurant');
+        throw Exception('Failed to load list photos');
       }
     } on DioError catch (e) {
       if (DioErrorType.receiveTimeout == e.type || DioErrorType.connectTimeout == e.type) {
         throw Exception("Server is not reachable. Please verify your internet connection and try again");
       } else if (DioErrorType.response == e.type) {
-        throw Exception('Failed to load list restaurant');
+        throw Exception('Failed to load list photos');
       } else if (DioErrorType.other == e.type) {
         if (e.message.contains('SocketException')) {
           throw Exception('Problem connecting to the server. Please try again.');
